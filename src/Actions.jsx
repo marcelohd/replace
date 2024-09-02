e0f5096380f4865ac872cf446715ea0e1e5df131
Marcelo Henrique <marcelohd02@gmail.com>
2024-09-02 01:50:53 +0000

    inclusao de botoes enviar e cancelar, e css

---
+++ src/Actions.jsx
@@ -0,0 +1,14 @@
+import "./Actions.css";
+
+export function Actions() {
+  return (
+    <div className="acoes">
+      <a href="#">
+        <p>Enviar</p>
+      </a>
+      <a href="#">
+        <p>Cancelar</p>
+      </a>
+    </div>
+  );
+}
