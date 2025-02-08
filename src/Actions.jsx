21719476c5a83baaa3d0e42ee5aae4962f6aa830
Marcelo Henrique <marcelohd02@gmail.com>
2025-02-08 20:54:13 +0000

    Inserindo botao de selecao para escolher a plataforma

--- src/Actions.jsx
+++ src/Actions.jsx
@@ -3,12 +3,21 @@ import "./Actions.css";
 export function Actions() {
   return (
     <div className="acoes">
-      <a href="#">
-        <p>Enviar</p>
-      </a>
-      <a href="#">
-        <p>Cancelar</p>
-      </a>
+      <div>
+        <select name="select">
+          <option value="1">Licitar Digital</option>
+          <option value="2">BLL, Compras-BR e VA Sistemas</option>
+          <option value="3">BBMNET - Bolsa Brasileira de Mercadorias</option>
+          <option value="4">Licitanet</option>
+          <option value="5">Banco do Brasil - Licitações-E</option>
+        </select>
+        <a href="#">
+          <p>Enviar</p>
+        </a>
+        <a href="#">
+          <p>Cancelar</p>
+        </a>
+      </div>
     </div>
   );
 }
